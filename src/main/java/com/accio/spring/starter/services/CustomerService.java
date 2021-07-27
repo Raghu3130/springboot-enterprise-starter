b383dc959cda8da6beea6c5373f90d473ba2829e
Piyush Patel <prpateldev@gmail.com>
2021-07-27 18:39:27 +0530

    Fixing code warnings

--- src/main/java/com/accio/spring/starter/services/CustomerService.java
+++ src/main/java/com/accio/spring/starter/services/CustomerService.java
@@ -6,15 +6,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CustomerService {
-    Customer create(Customer toCreate) throws Exception;
+    Customer create(Customer toCreate) throws RuntimeException;
 
     List<Customer> findAll();
 
-    Customer findById(String id) throws Exception;
+    Customer findById(String id) throws RuntimeException;
 
     Optional<Customer> findByIdNoException(String id);
 
-    Boolean update(String id, Customer toUpdate) throws Exception;
+    Boolean update(String id, Customer toUpdate) throws RuntimeException;
 
     Boolean delete(String id);
 
